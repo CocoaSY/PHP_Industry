34b14517bcb3d6ab7721a2e4c4369bca827a1291
CocoaSY <757521389@qq.com>
2016-10-27 17:36:16 +0800

    config

---
+++ Application/Common/Conf/DBConf.php
@@ -0,0 +1,15 @@
+<?php
+
+function DBConfig(){
+    return array(
+        /* 数据库设置 */
+        'DB_TYPE'               =>  'mysql',         // 数据库类型
+        'DB_HOST'               =>  'localhost',     // 服务器地址
+        'DB_NAME'               =>  'yyblog',    // 数据库名
+        'DB_USER'               =>  'yyblog',          // 用户名
+        'DB_PWD'                =>  'aa123456L',              // 密码
+        'DB_PORT'               =>  '3306',          // 端口
+        'DB_PREFIX'             =>  'z_',            // 数据库表前缀
+
+    );
+}
