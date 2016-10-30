3fb5a04fbc009595df935e5ab971ff742d854045
CocoaSY <757521389@qq.com>
2016-10-30 23:51:59 +0800

    话题分类的异步操作

--- Application/Admin/Controller/TopicController.class.php
+++ Application/Admin/Controller/TopicController.class.php
@@ -8,10 +8,20 @@
 
 namespace Admin\Controller;
 
+
 use Admin\Model\TopicCateModel;
 use Admin\Model\TopicModel;
 
 class TopicController extends BaseController{
+
+    private $topicM;
+    private $topicCateM;
+
+    public function __construct(){
+        parent::__construct();
+        $this->topicM = new TopicModel();
+        $this->topicCateM = new TopicCateModel();
+    }
 
     public function index(){
         $topicM = new TopicModel();
@@ -34,9 +44,7 @@ class TopicController extends BaseController{
     }
 
 
-    /**
-     * 异步添加话题
-     */
+    /** 异步请求 话题 */
     public function addTopic(){
 
         $topicM = new TopicModel();
@@ -67,6 +75,8 @@ class TopicController extends BaseController{
         }
     }
 
+
+    /** 异步请求 话题分类*/
     public function addCategory(){
 
         $topiccateM = new TopicCateModel();
@@ -87,6 +97,50 @@ class TopicController extends BaseController{
             $this->ajaxReturn($res);
         }
 
+    }
+
+    public function getCategory(){
+        $cid = I("cid");
+        $category = $this->topicCateM->where(array("t_id"=>$cid))->find();
+        $this->ajaxReturn($category);
+    }
+
+    public function updateCategory(){
+        if(IS_POST){
+            $data['t_id'] = I("cid");
+            $data['name'] = I("name");
+            $data['ename'] = I("ename");
+            $rs = $this->topicCateM->where(array("t_id"=>$data['t_id']))->save($data);
+            if($rs){
+                $cate = $this->topicCateM->where(array("t_id"=>$rs))->find();
+                $res['rs']=1;
+                $res['msg']="数据更新成功";
+                $res['info']=$cate;
+            }else{
+                $res['rs']=0;
+                $res['msg']="数据更新失败";
+                $res['info']=0;
+            }
+            $this->ajaxReturn($res);
+        }
+    }
+
+    public function delCategory(){
+        if($_POST){
+            $cid = I("cid");
+
+            $rs = $this->topicCateM->where(array('t_id'=>$cid))->delete();
+            if($rs){
+                $res['rs'] = 1;
+                $res['msg'] = "删除成功";
+                $res['info'] = 1;
+            }else{
+                $res['rs'] = 0;
+                $res['msg'] = "删除失败";
+                $res['info'] = 0;
+            }
+            $this->ajaxReturn($res);
+        }
     }
 
 }
